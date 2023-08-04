ff11f53e8139513579fd0b315553d3a277786e80
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-04 15:20:08 -0500

    Person oop excercise

---
+++ src/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    //properties and fields
+    private String name;
+    //constructors
+    public Person(String name){
+        this.name = name;
+    }
+    //getters and setters
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    //other methods
+
+    public void sayHello(){
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        Person jose = new Person("person");
+        System.out.println(jose.name);
+        jose.setName("Jose De La Luz");
+        System.out.println(jose.name);
+        jose.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+
+    }
+
+}
