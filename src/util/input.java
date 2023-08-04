fc92d1abacd9eda5bdbcf70f59a21a1da089eefa
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-04 16:41:45 -0500

    custom scanner input class

--- src/util/input.java
+++ src/util/input.java
@@ -1,2 +1,49 @@
-package util;public class input {
+package util;
+
+import java.util.Scanner;
+
+public class input {
+    //properties and fields
+    private final Scanner sc = new Scanner(System.in);
+
+    //constructors
+    public Scanner sc(){
+        return this.sc;
+    }
+
+    //getters and setters
+    public String getString (){
+        System.out.println("enter a string");
+        return this.sc.nextLine();
+    }
+    public boolean yesNo (String sc){
+       return sc.equals("y");
+    }
+    public int getInt(int min, int max){
+        Scanner sc = new Scanner(System.in);
+        int userInput;
+        do {
+        System.out.println("enter a number between " + min +" and " + max);
+        userInput = sc.nextInt();
+
+        } while(userInput < min || userInput > max);
+        return userInput;
+    }
+    public int getInt(){
+        return sc.nextInt();
+    }
+    public double getDouble(double min, double max){
+        Scanner sc = new Scanner(System.in);
+        double userInput;
+        do {
+            System.out.println("enter a number between " + min +" and " + max);
+            userInput = sc.nextDouble();
+
+        } while(userInput < min || userInput > max);
+        return userInput;
+    }
+    public double getDouble(){
+        return sc.nextDouble();
+    }
+
 }
