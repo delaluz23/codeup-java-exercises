5643eaf8e456e51377d4e1288b791880cf994188
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-07 10:49:14 -0500

    shapes excercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,2 +1,15 @@
-package shapes;public class CircleApp {
+package shapes;
+
+import util.input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        input sc = new input();
+        do {
+            System.out.println("enter your circle radius");
+            double radius = sc.getDouble();
+            Circle myCircle = new Circle(radius);
+            System.out.printf("the area of your circle is %f, the circumference is %f.%n", myCircle.getArea(), myCircle.getCircumference());
+        } while (sc.yesNo("do you want to try again? [Y/N]"));
+    }
 }
