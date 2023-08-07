e195a1368f1824afdd518bde0fbbb07d0dd2a52d
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-07 10:48:57 -0500

    refactored after walkthrough

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -6,34 +6,37 @@ public class InputTest {
 
 
         //get string input getter
+        System.out.println("enter a string");
         String word = sc.getString();
         System.out.println(word);
 
         //boolean yesNo instance
-        String cont;
         do {
             System.out.println("hi");
-            System.out.println("conintue? [Y/N]");
-            cont = sc.getString();
-        } while (sc.yesNo(cont));
+            System.out.println("continue? [Y/N]");
+        } while (sc.yesNo());
+
+        //get int w get min and max
+        System.out.println("enter a int number between one and ten");
+        System.out.println(sc.getInt(1,10));
+
+//        //get int min and max
+//        System.out.println("enter a number");
+//        int num = sc.getInt();
+//        System.out.println("yout int is : " + num);
 
 
-        //get int min and max
-        int num = sc.getInt(1,20);
+        System.out.println("enter a double number between one and ten");
+        System.out.println(sc.getDouble(1,10));
 
-        //get int
-        System.out.println("enter a number");
-        int num2 = sc.getInt();
-        System.out.println(num2);
-
-
-        //get double min and max
-        double dblnum = sc.getDouble(1,20);
-
-        //get int
-        System.out.println("enter a number");
-        double dblnum2 = sc.getDouble();
-        System.out.println(num2);
+//        //get double min and max
+//        double dblnum = sc.getDouble(1,20);
+//
+//        //get int
+//        System.out.println("enter a number");
+//        double dblnum2 = sc.getDouble();
+//        System.out.println();
+        System.out.println(sc.getString("bananas"));
 
     }
 
--- src/util/input.java
+++ src/util/input.java
@@ -4,46 +4,104 @@ import java.util.Scanner;
 
 public class input {
     //properties and fields
-    private final Scanner sc = new Scanner(System.in);
+    private Scanner sc;
 
     //constructors
-    public Scanner sc(){
-        return this.sc;
+    public input() {
+        this.sc = new Scanner(System.in);
     }
 
     //getters and setters
-    public String getString (){
-        System.out.println("enter a string");
+    public String getString() {
+//        System.out.println("enter a string");
         return this.sc.nextLine();
     }
-    public boolean yesNo (String sc){
-       return sc.equals("y");
-    }
-    public int getInt(int min, int max){
-        Scanner sc = new Scanner(System.in);
-        int userInput;
-        do {
-        System.out.println("enter a number between " + min +" and " + max);
-        userInput = sc.nextInt();
 
-        } while(userInput < min || userInput > max);
-        return userInput;
-    }
-    public int getInt(){
-        return sc.nextInt();
-    }
-    public double getDouble(double min, double max){
-        Scanner sc = new Scanner(System.in);
-        double userInput;
-        do {
-            System.out.println("enter a number between " + min +" and " + max);
-            userInput = sc.nextDouble();
-
-        } while(userInput < min || userInput > max);
-        return userInput;
-    }
-    public double getDouble(){
-        return sc.nextDouble();
+    public String getString(String prompt){
+        System.out.println(prompt);
+        return this.getString();
     }
 
+    //    public boolean yesNo(String sc) {
+//        return sc.equalsIgnoreCase("y");
+//    }
+    //walkthrough version
+    public boolean yesNo() {
+        return this.getString().toLowerCase().startsWith("y");
+    }
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        return this.getString().toLowerCase().startsWith("y");
+    }
+
+    //    public int getInt(int min, int max) {
+//        Scanner sc = new Scanner(System.in);
+//        int userInput;
+//        do {
+//            System.out.println("enter a number between " + min + " and " + max);
+//            userInput = sc.nextInt();
+//
+//        } while (userInput < min || userInput > max);
+//        return userInput;
+//    }
+//
+//    public int getInt() {
+//        return sc.nextInt();
+//    }
+    //walkthrough
+    public int getInt() {
+        try {
+            return Integer.parseInt(this.getString());
+        } catch (NumberFormatException e) {
+            System.out.println("You must enter a whole number.");
+            return this.getInt();
+        }
+    }
+
+    public int getInt(int min, int max) {
+        int userint = this.getInt();
+        if (userint >= min && userint <= max) {
+            return userint;
+        } else {
+            System.out.println("must be between " + min + " and " + max + ".");
+            return getInt(min, max);
+        }
+    }
+
+
+//    public double getDouble(double min, double max) {
+//        Scanner sc = new Scanner(System.in);
+//        double userInput;
+//        do {
+//            System.out.println("enter a number between " + min + " and " + max);
+//            userInput = sc.nextDouble();
+//
+//        } while (userInput < min || userInput > max);
+//        return userInput;
+//    }
+//
+//    public double getDouble() {
+//        return sc.nextDouble();
+//    }
+
+
+    //walkthrough
+    public double getDouble() {
+        try {
+            return Double.parseDouble(this.getString());
+        } catch (NumberFormatException e) {
+            System.out.println("You must enter a number.");
+            return this.getDouble();
+        }
+    }
+
+    public double getDouble(double min, double max) {
+        double userDouble = this.getDouble();
+        if (userDouble >= min && userDouble <= max) {
+            return userDouble;
+        } else {
+            System.out.println("must be between " + min + " and " + max + ".");
+            return getDouble(min, max);
+        }
+    }
 }
